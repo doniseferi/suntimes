a44b72b00b40d3935b655604a090fb9291a7d492
Edonis Seferi <doni.seferi@gmail.com>
2019-07-30 07:50:14 +0100

    tests added, implementation added

--- src/time/getMidday.test.js
+++ src/time/getMidday.test.js
@@ -15,12 +15,11 @@ const todaysTestData = [
   model(-0.190554, 1, 13.12),
   model(-47.921822, -3, 12.30),
   model(149.131393, 10, 12.17),
-  model(10.752245, 2, 13.39),
-  model(21.165319, 2, 13.70)
+  model(10.752245, 2, 13.39)
 ];
 
 suite('Get midday', () => {
-  test('returns the correct time in hours for London, England on 28/07/2019', () => {
+  test('returns the correct time in hours for the 28/07/2019', () => {
     todaysTestData.forEach(expected => {
       const { latitude, date, timeZone, middayInHours } = expected;
       const midday = getMidday(latitude, date, timeZone);
