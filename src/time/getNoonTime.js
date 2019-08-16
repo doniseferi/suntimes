bf5f36d157ecb3b006b17f288934ac99281bc236
Edonis Seferi <edonis.seferi@asos.com>
2019-08-16 15:25:24 +0100

    qci

--- src/time/getNoonTime.js
+++ src/time/getNoonTime.js
@@ -1,10 +1,20 @@
 const getNoonTimeBuilder = (equationOfTime) => {
-  const getNoonTime = (date, longitude) => {
+  const getNoonTimeInHours = (date, longitude) => {
     return 12 - (longitude / 15 + equationOfTime(date).hours);
   };
 
+  const toUtcDate = (date, decimalTimeInHours) => {
+    const minutesInPercentage = decimal % 1;
+    const minutes = minutesInPercentage * 60;
+    const secondsInPercentage = minutes % 1;
+    const seconds = secondsInPercentage * 60;
+    const day = date.get
+
+    const date = new Date()
+  };
+
   return Object.freeze({
-    getNoonTime: (date, longitude) => getNoonTime(date, longitude)
+    getNoonTime: (date, longitude) => getNoonTimeInHours(date, longitude)
   });
 };
 
--- src/time/getNoonTime.test.js
+++ src/time/getNoonTime.test.js
@@ -6,24 +6,13 @@ import januaryNoonExpected from './januaryNoonExpected.json';
 suite('Get Noon Time', () => {
   test('returns noon time within a +/- 1 minute accuracy', () => {
     januaryNoonExpected.forEach(expected => {
-      const expectedTime = expected.ExpectedTime;
-      const longitude = expected.Longitude;
-      const actualTimeInMinutes = hoursToMiutes(getNoonTime(new Date(2019, 0, 1), longitude));
-      const expectedTimeInMinutes = hoursToMiutes(timeToHours(expectedTime.split(' ')[1]));
+      const expectedTime = new Date(expected.ExpectedTime).toLocaleTimeString('en-GB', { timeZone: 'UTC' });
+      const actualTime = getNoonTime(new Date(2019, 0, 1), expected.Longitude);
+      const actualTimeInMinutes = hoursToMiutes(actualTime);
+      const expectedTimeInMinutes = hoursToMiutes(timeToHours(expectedTime));
       const set = [actualTimeInMinutes, expectedTimeInMinutes];
       const difference = Math.max.apply(null, set) - Math.min.apply(null, set);
       assert.closeTo(difference, 0, 1);
     });
   });
-
-  const timeToHours = (time) => {
-    const [h, m, s] = time.split(':');
-    const hours = parseFloat(h);
-    const minutes = parseFloat(m) * (1 / 60);
-    const seconds = parseFloat(s) * (1 / 3600);
-
-    return hours + minutes + seconds;
-  };
-
-  const hoursToMiutes = (hours) => hours * 60;
 });
