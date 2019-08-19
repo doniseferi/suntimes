0d8862fef0eb92afe4e24109fbbf7ee285994f1a
Edonis Seferi <edonis.seferi@asos.com>
2019-08-19 14:16:13 +0100

    Updated test files

--- src/time/getNoonTime.test.js
+++ src/time/getNoonTime.test.js
@@ -1,16 +1,27 @@
 import { suite, test } from 'mocha';
 import { assert } from 'chai';
 import { getNoonTime } from './index';
-import januaryNoonExpected from './januaryNoonExpected.json';
+import januaryNoonExpected from './testFiles/januaryNoonExpected.json';
+import julyNoonExpected from './testFiles/julyNoonExpected.json';
 
 suite('Get Noon Time', () => {
-  test('returns noon time within a +/- 1 minute accuracy', () => {
+  test('returns noon time within a +/- 1 minute accuracy for the 01/01/2019', () => {
     januaryNoonExpected.forEach(expected => {
       const expectedTime = new Date(expected.ExpectedTime);
       const targetDate = new Date(2019, 0, 1);
       const actualTime = getNoonTime(targetDate, expected.Longitude);
-      const differenceInMiliseconds = Math.abs(expectedTime - actualTime);
-      const differenceInSeconds = Math.round(((differenceInMiliseconds % 86400000) % 3600000) / 60000);
+      const differenceInSeconds =
+      (expectedTime.getTime() - actualTime.getTime()) / 1000;
+      assert.closeTo(differenceInSeconds, 0, 60);
+    });
+  });
+  test('returns noon time within a +/- 1 minute accuracy for the 01/07/2019', () => {
+    julyNoonExpected.forEach(expected => {
+      const expectedTime = new Date(expected.ExpectedTime);
+      const targetDate = new Date(2019, 6, 1);
+      const actualTime = getNoonTime(targetDate, expected.Longitude);
+      const differenceInSeconds =
+        (expectedTime.getTime() - actualTime.getTime()) / 1000;
       assert.closeTo(differenceInSeconds, 0, 60);
     });
   });
