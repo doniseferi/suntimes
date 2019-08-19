0f3cf812b0c974a520d4beb6efc8509be8f81783
Edonis Seferi <edonis.seferi@asos.com>
2019-08-19 13:26:57 +0100

    Added ms to getnoontime builder

--- src/time/getNoonTime.js
+++ src/time/getNoonTime.js
@@ -4,15 +4,18 @@ const getNoonTimeBuilder = (equationOfTime) => {
   };
 
   const toDate = (date, decimalTimeInHours) => {
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
     const hour = decimalTimeInHours;
     const minutesInPercentage = decimalTimeInHours % 1;
     const minutes = minutesInPercentage * 60;
     const secondsInPercentage = minutes % 1;
     const seconds = secondsInPercentage * 60;
-    const day = date.getDate();
-    const month = date.getMonth();
-    const year = date.getFullYear();
-    return new Date(year, month, day, hour, minutes, seconds);
+    const miliSecondsInPercentage = seconds % 1;
+    const miliSeconds = miliSecondsInPercentage * 1000;
+    return new Date(year, month, day, hour, minutes, seconds, miliSeconds);
   };
 
   return Object.freeze({
