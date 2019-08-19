dfe2a877830fc0b476cda6a76a2f31cd5b5a7a11
Edonis Seferi <edonis.seferi@asos.com>
2019-08-19 09:20:10 +0100

    qfci

--- src/time/getNoonTime.js
+++ src/time/getNoonTime.js
@@ -3,18 +3,20 @@ const getNoonTimeBuilder = (equationOfTime) => {
     return 12 - (longitude / 15 + equationOfTime(date).hours);
   };
 
-  const toUtcDate = (date, decimalTimeInHours) => {
-    const minutesInPercentage = decimal % 1;
+  const toDate = (date, decimalTimeInHours) => {
+    const hour = decimalTimeInHours;
+    const minutesInPercentage = decimalTimeInHours % 1;
     const minutes = minutesInPercentage * 60;
     const secondsInPercentage = minutes % 1;
     const seconds = secondsInPercentage * 60;
-    const day = date.get
-
-    const date = new Date()
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    return new Date(year, month, day, hour, minutes, seconds);
   };
 
   return Object.freeze({
-    getNoonTime: (date, longitude) => getNoonTimeInHours(date, longitude)
+    getNoonTime: (date, longitude) => toDate(date, getNoonTimeInHours(date, longitude))
   });
 };
 
--- src/time/getNoonTime.test.js
+++ src/time/getNoonTime.test.js
@@ -6,13 +6,12 @@ import januaryNoonExpected from './januaryNoonExpected.json';
 suite('Get Noon Time', () => {
   test('returns noon time within a +/- 1 minute accuracy', () => {
     januaryNoonExpected.forEach(expected => {
-      const expectedTime = new Date(expected.ExpectedTime).toLocaleTimeString('en-GB', { timeZone: 'UTC' });
-      const actualTime = getNoonTime(new Date(2019, 0, 1), expected.Longitude);
-      const actualTimeInMinutes = hoursToMiutes(actualTime);
-      const expectedTimeInMinutes = hoursToMiutes(timeToHours(expectedTime));
-      const set = [actualTimeInMinutes, expectedTimeInMinutes];
-      const difference = Math.max.apply(null, set) - Math.min.apply(null, set);
-      assert.closeTo(difference, 0, 1);
+      const expectedTime = new Date(expected.ExpectedTime);
+      const targetDate = new Date(2019, 0, 1);
+      const actualTime = getNoonTime(targetDate, expected.Longitude);
+      const difference = Math.abs(expectedTime - actualTime);
+      const differenceInMinutes = Math.round(((difference % 86400000) % 3600000) / 60000);
+      assert.closeTo(differenceInMinutes, 0, 1);
     });
   });
 });
