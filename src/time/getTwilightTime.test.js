38ad623938f3e1ecbc96fa21ca3c676a892d8c3a
Edonis Seferi <edonis.seferi@asos.com>
2019-09-19 16:14:36 +0100

    get astro dawn tests

--- src/time/getTwilightTime.test.js
+++ src/time/getTwilightTime.test.js
@@ -3,14 +3,17 @@ import { assert } from 'chai';
 import { getTwilightAstronomicalStartTime } from './index';
 import januaryTwilightExpected from './testData/utcJanTwilight.json';
 
-suite('Get astronomical dawn date time', () => {
-  test('returns with an accuracy of x +/- seconds', () => {
+suite('Get twilight date time', () => {
+  test('returns the astronimcal dawn start time with an accuracy of 60 +/- seconds', () => {
     januaryTwilightExpected.forEach(expected => {
       const { ExpectedAstronomicalDawnUtc, Latitude, Longitude } = expected;
       const expectedDateTime = new Date(ExpectedAstronomicalDawnUtc);
 
-      const actualDateTime =
-        getTwilightAstronomicalStartTime(new Date(2022, 0, 1), Latitude, Longitude);
+      const actualDateTime = getTwilightAstronomicalStartTime(
+        new Date(2022, 0, 1),
+        Latitude,
+        Longitude
+      );
 
       const differenceInSeconds =
         (expectedDateTime.getTime() - actualDateTime.getTime()) / 1000;
