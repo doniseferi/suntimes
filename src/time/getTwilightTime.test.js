4d14115409f7b2f458d34479853286ba977421eb
Edonis Seferi <edonis.seferi@asos.com>
2019-10-08 17:22:22 +0100

    qci

--- src/time/getTwilightTime.test.js
+++ src/time/getTwilightTime.test.js
@@ -24,13 +24,15 @@ suite('Get twilight date time', () => {
       if (differenceInSeconds > 60 || differenceInSeconds < -60) {
         const twilightUrl = `https://www.timeanddate.com/sun/@${Latitude},${Longitude}?month=1&year=2022`;
         const timeZoneUrl = `https://www.timeanddate.com/time/zone/@${Latitude},${Longitude}`;
+        const some = `https://www.timeanddate.com/worldclock/converter.html?iso=${actualDateTime.getUTCFullYear()}${actualDateTime.setUTCMonth}${actualDateTime.getUTCDate()}T${actualDateTime.getUTCHours()}${actualDateTime.getUTCMinutes()}${actualDateTime.getUTCSeconds()}&p1=1440`;
         accum.push(
           JSON.stringify({
             twilightUrl,
             timeZoneUrl,
             actualDateTime,
             differenceInSeconds,
-            expected
+            expected,
+            some
           })
         );
       }
@@ -40,7 +42,7 @@ suite('Get twilight date time', () => {
 
   test('returns the nautical dawn start time with an accuracy of 60 +/- seconds for 01/01/2022', () => {
     const accum = [];
-    januaryTwilightExpected.forEach(expected => {
+    januaryTwilightExpected.filter(x => x.ExpectedCivilDawnUtc).forEach(expected => {
       const { ExpectedNauticalDawnUtc, Latitude, Longitude } = expected;
       const expectedDateTime = new Date(ExpectedNauticalDawnUtc);
 
@@ -56,8 +58,10 @@ suite('Get twilight date time', () => {
       if (differenceInSeconds > 60 || differenceInSeconds < -60) {
         const twilightUrl = `https://www.timeanddate.com/sun/@${Latitude},${Longitude}?month=1&year=2022`;
         const timeZoneUrl = `https://www.timeanddate.com/time/zone/@${Latitude},${Longitude}`;
+        const some = `https://www.timeanddate.com/worldclock/converter.html?iso=20220101T${actualDateTime.getUTCHours()}${actualDateTime.getUTCMinutes()}${actualDateTime.getUTCSeconds()}&p1=1440`;
         accum.push(
           JSON.stringify({
+            some,
             twilightUrl,
             timeZoneUrl,
             actualDateTime,
@@ -73,7 +77,7 @@ suite('Get twilight date time', () => {
   test('returns the civil dawn start time with an accuracy of 60 +/- seconds for 01/01/2022', () => {
     const accum = [];
     januaryTwilightExpected.forEach(expected => {
-      const { ExpectedCivilDawnUtc, Latitude, Longitude } = expected;
+      const { ExpectedCivilDawnUtc, Latitude, Longitude, City, Country } = expected;
       const _expectedDateTime = new Date(ExpectedCivilDawnUtc);
 
       const _actualDateTime = getTwilightCivilStartDateTime(
@@ -86,16 +90,14 @@ suite('Get twilight date time', () => {
         (_expectedDateTime.getTime() - _actualDateTime.getTime()) / 1000;
 
       if (differenceInSeconds > 60 || differenceInSeconds < -60) {
+        const some = `https://www.timeanddate.com/worldclock/converter.html?iso=20220101T${_actualDateTime.getUTCHours()}${_actualDateTime.getUTCMinutes()}${_actualDateTime.getUTCSeconds()}&p1=1440`;
         const twilightUrl = `https://www.timeanddate.com/sun/@${Latitude},${Longitude}?month=1&year=2022`;
-        const timeZoneUrl = `https://www.timeanddate.com/time/zone/@${Latitude},${Longitude}`;
-        const _ex = expected.ExpectedCivilDawnUtc;
+        const country = `${City}, ${Country}`;
         accum.push(JSON.stringify({
+          some,
+          country,
           twilightUrl,
-          timeZoneUrl,
-          _actualDateTime,
-          _ex,
-          ExpectedCivilDawnUtc,
-          differenceInSeconds
+          _actualDateTime
         }));
       }
     });
