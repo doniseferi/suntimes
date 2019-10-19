6a01ca1a7f6acb158d7a326b9e7c59e08affbcca
Edonis Seferi <doni.seferi@gmail.com>
2019-10-19 15:03:23 +0100

    jan twilight complete

--- src/time/getTwilightTime.test.js
+++ src/time/getTwilightTime.test.js
@@ -4,7 +4,9 @@ import {
   getAstronomicalDawnStartDateTime,
   getNauticalDawnStartDateTime,
   getCivilDawnStartDateTime,
-  getAstronomicalDuskStartDateTime
+  getAstronomicalDuskStartDateTime,
+  getNauticalDuskStartDateTime,
+  getCivilDuskStartDateTime
 } from './index';
 import januaryTwilightExpected from './testData/utcJanTwilight.json';
 
@@ -82,6 +84,65 @@ suite('Get twilight dusk date time', () => {
         (expectedDateTime.getTime() - actualDateTime.getTime()) / 1000;
 
       assert.closeTo(differenceInSeconds, 0, 60);
+    });
+  });
+
+  test('returns the nautical dusk start time with an accuracy of 60 +/- seconds for 01/01/2022', () => {
+    januaryTwilightExpected.forEach(expected => {
+      const { ExpectedNauticalDuskUtc, Latitude, Longitude } = expected;
+      const expectedDateTime = new Date(ExpectedNauticalDuskUtc);
+
+      const actualDateTime = getNauticalDuskStartDateTime(
+        jan,
+        Latitude,
+        Longitude
+      );
+
+      const differenceInSeconds =
+        (expectedDateTime.getTime() - actualDateTime.getTime()) / 1000;
+
+      assert.closeTo(differenceInSeconds, 0, 60);
+    });
+  });
+
+  test('returns the civil dusk start time with an accuracy of 60 +/- seconds for 01/01/2022', () => {
+    // const formatDate = date =>
+    //   date.getFullYear() +
+    //   ('0' + (date.getMonth() + 1)).slice(-2) +
+    //   ('0' + date.getDate()).slice(-2) +
+    //   'T' +
+    //   (date.getHours() < 10 ? '0' : '') +
+    //   date.getHours() +
+    //   (date.getMinutes() < 10 ? '0' : '') +
+    //   date.getMinutes() +
+    //   (date.getSeconds() < 10 ? '0' : '') +
+    //   date.getSeconds();
+
+    januaryTwilightExpected.forEach(expected => {
+      const { ExpectedCivilDuskUtc, Latitude, Longitude } = expected;
+      const expectedDateTime = new Date(ExpectedCivilDuskUtc);
+
+      const actualDateTime = getCivilDuskStartDateTime(
+        jan,
+        Latitude,
+        Longitude
+      );
+
+      const differenceInSeconds = (expectedDateTime.getTime() - actualDateTime.getTime()) / 1000;
+
+      assert.closeTo(differenceInSeconds, 0, 60);
+      // if (differenceInSeconds > 60 || differenceInSeconds < -60) {
+      //   const strDate = formatDate(actualDateTime);
+      //   const utc = `https://www.timeanddate.com/worldclock/converter.html?iso=${strDate}00&p1=1440`;
+      //   const url = `https://www.timeanddate.com/sun/@${Latitude},${Longitude}?month=1&year=2022`;
+      //   x.push({
+      //     url,
+      //     utc,
+      //     differenceInSeconds,
+      //     actualDateTime,
+      //     expected
+      //   });
+      // }
     });
   });
 });
