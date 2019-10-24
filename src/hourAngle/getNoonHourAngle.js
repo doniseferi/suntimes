bd413451ef8f7ebd5bbdae5924ea72faa709e230
Edonis Seferi <doni.seferi@gmail.com>
2019-10-24 21:10:01 +0100

    file renaming

---
+++ src/hourAngle/getNoonHourAngle.js
@@ -0,0 +1,30 @@
+const getNoonHourAngleFactory = (equationOfTime, toUtcDateTime) => {
+  const getNoonHourAngle = (date, longitude) => {
+    let hour = 12 - (getHourDifference(longitude) + getEquationOfTimeInHours(equationOfTime, date));
+    let offsetInDays = 0;
+    if (hour < 0) {
+      hour = hour + 24;
+      offsetInDays = -1;
+    }
+    if (hour >= 24) {
+      hour = hour - 24;
+      offsetInDays = +1;
+    }
+    return {
+      hour,
+      offsetInDays
+    };
+  };
+
+  const getEquationOfTimeInHours = (equationOfTime, date) => equationOfTime(date) / 60;
+
+  const getHourDifference = longitude => longitude / 15;
+
+  return Object.freeze({
+    getNoonDateTimeUtc: (date, longitude) =>
+      toUtcDateTime(date, getNoonHourAngle(date, longitude)),
+    getNoonHourAngle: (date, longitude) => getNoonHourAngle(date, longitude)
+  });
+};
+
+export default getNoonHourAngleFactory;
