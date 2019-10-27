0294363e35ea2c7328655ae95213f11014699fcb
Edonis Seferi <doni.seferi@gmail.com>
2019-10-27 21:08:51 +0000

    typo

--- src/time/getDateTimeAtAngle.js
+++ src/time/getDateTimeAtAngle.js
@@ -1,9 +1,9 @@
 const getDateTimeUtcFactory = (getNoonDateTimeUtc, getHourAngleSinceNoon) => {
-  const throwUnsatisfiedDependencyError = (dependencyName, depdencyParams, depedencyReturnType) => {
+  const throwUnsatisfiedDependencyError = (dependencyName, dependencyParams, depedencyReturnType) => {
     throw new Error(
       `Unsatisfied Dependency Error:
       Please provide a function for ${dependencyName}.
-      This dependency takes in params ${depdencyParams}
+      This dependency takes in params ${dependencyParams}
       and returns ${depedencyReturnType}`
     );
   };
