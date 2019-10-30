ffdb50d7db370745ef17b207016f6e2dccc904b4
Edonis Seferi <doni.seferi@gmail.com>
2019-10-30 21:39:22 +0000

    declination update and formatting

--- src/declination/getDeclinationOfTheSun.js
+++ src/declination/getDeclinationOfTheSun.js
@@ -32,21 +32,16 @@ const declinationOfTheSunFactory = (sine, cosine, arcsine, getDayOfTheYear) => {
     if (!date) {
       throw new Error('Please provide a valid date');
     }
+
     const approxDaysAfterDecemberSolstice = getDayOfTheYear(date) - 1;
-    const declinationOfTheSunInRadians = declinationOfTheSunAlgorithm(
-      approxDaysAfterDecemberSolstice
-    );
-    return -arcsine(0.39779 * declinationOfTheSunInRadians);
+    const declinationOfTheSunInRadians = declinationOfTheSunAlgorithm(approxDaysAfterDecemberSolstice);
+    return arcsine(-0.39779 * declinationOfTheSunInRadians);
   };
 
   const declinationOfTheSunAlgorithm = approxDaysAfterDecemberSolstice => {
     const zero360over365p24 = 0.98565;
-    const eclipticLongitude = sine(
-      zero360over365p24 * (approxDaysAfterDecemberSolstice - 2)
-    );
-    const formula =
-      zero360over365p24 * (approxDaysAfterDecemberSolstice + 10) +
-      1.914 * eclipticLongitude;
+    const eclipticLongitude = sine(zero360over365p24 * (approxDaysAfterDecemberSolstice - 2));
+    const formula = zero360over365p24 * (approxDaysAfterDecemberSolstice + 10) + 1.914 * eclipticLongitude;
     return cosine(formula);
   };
 
