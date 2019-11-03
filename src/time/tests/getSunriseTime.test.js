0216cd608c13b8bae331a91e34ab654cfcff7379
Edonis Seferi <doni.seferi@gmail.com>
2019-11-03 21:05:08 +0000

    twilights added to generic test

--- src/time/tests/getSunriseTime.test.js
+++ src/time/tests/getSunriseTime.test.js
@@ -2,17 +2,25 @@ import { test } from 'mocha';
 import { assert } from 'chai';
 import {
   getSunriseDateTimeUtc,
-  getSunsetDateTimeUtc
+  getSunsetDateTimeUtc,
+  getAstronomicalDawnStartDateTimeUtc,
+  getNauticalDawnStartDateTimeUtc,
+  getCivilDawnStartDateTimeUtc,
+  getAstronomicalDuskStartDateTimeUtc,
+  getNauticalDuskStartDateTimeUtc,
+  getCivilDuskStartDateTimeUtc
 } from '../../../index';
 import januarySunriseExpected from './utcJanSunrise.json';
 import julySunriseExpected from './utcJulySunrise.json';
 import januarySunsetExpected from './utcJanSunset.json';
 import julySunsetExpected from './utcJulySunset.json';
+import januaryTwilightExpected from './utcJanTwilight.json';
 
-const testGeneric = (targetDate, testData, functionUnderTest) => {
+const testGeneric = (targetDate, testData, functionUnderTest, name) => {
   testData.forEach(expected => {
-    const { ExpectedTimeUtc, Latitude, Longitude } = expected;
-    const expectedTime = new Date(ExpectedTimeUtc);
+    const { Latitude, Longitude } = expected;
+    const expectedDateTimeUtc = expected[name];
+    const expectedTime = new Date(expectedDateTimeUtc);
     const actualDateTime = functionUnderTest(targetDate, Latitude, Longitude);
     const differenceInSeconds =
       (expectedTime.getTime() - actualDateTime.getTime()) / 1000;
@@ -20,24 +28,71 @@ const testGeneric = (targetDate, testData, functionUnderTest) => {
   });
 };
 
-const januaryTestModel = (set, functionUnderTest) =>
-  model(new Date(2019, 0, 1), set, functionUnderTest);
-const julyTestModel = (set, functionUnderTest) =>
-  model(new Date(2019, 6, 1), set, functionUnderTest);
-const model = (date, set, functionUnderTest) => ({
+const jan = new Date(2019, 0, 1);
+const jul = new Date(2019, 6, 1);
+
+const model = (date, set, expectedPropertyName, functionUnderTest) => ({
   date,
   expectedSet: set,
+  expectedPropertyName,
   functionUnderTest
 });
 
 test('returns times time within a +/- 1 minute accuracy', () => {
   const testData = [
-    januaryTestModel(januarySunriseExpected, getSunriseDateTimeUtc),
-    julyTestModel(julySunriseExpected, getSunriseDateTimeUtc),
-    januaryTestModel(januarySunsetExpected, getSunsetDateTimeUtc),
-    julyTestModel(julySunsetExpected, getSunsetDateTimeUtc)
+    model(
+      jan,
+      januarySunriseExpected,
+      'ExpectedTimeUtc',
+      getSunriseDateTimeUtc
+    ),
+    model(jul, julySunriseExpected, 'ExpectedTimeUtc', getSunriseDateTimeUtc),
+    model(jan, januarySunsetExpected, 'ExpectedTimeUtc', getSunsetDateTimeUtc),
+    model(jul, julySunsetExpected, 'ExpectedTimeUtc', getSunsetDateTimeUtc),
+    model(
+      new Date(2022, 0, 1),
+      januaryTwilightExpected,
+      'ExpectedAstronomicalDawnUtc',
+      getAstronomicalDawnStartDateTimeUtc
+    ),
+    model(
+      new Date(2022, 0, 1),
+      januaryTwilightExpected,
+      'ExpectedNauticalDawnUtc',
+      getNauticalDawnStartDateTimeUtc
+    ),
+    model(
+      new Date(2022, 0, 1),
+      januaryTwilightExpected,
+      'ExpectedCivilDawnUtc',
+      getCivilDawnStartDateTimeUtc
+    ),
+    model(
+      new Date(2022, 0, 1),
+      januaryTwilightExpected,
+      'ExpectedAstronomicalDuskUtc',
+      getAstronomicalDuskStartDateTimeUtc
+    ),
+    model(
+      new Date(2022, 0, 1),
+      januaryTwilightExpected,
+      'ExpectedNauticalDuskUtc',
+      getNauticalDuskStartDateTimeUtc
+    ),
+    model(
+      new Date(2022, 0, 1),
+      januaryTwilightExpected,
+      'ExpectedCivilDuskUtc',
+      getCivilDuskStartDateTimeUtc
+    )
+
   ];
   testData.forEach(set =>
-    testGeneric(set.date, set.expectedSet, set.functionUnderTest)
+    testGeneric(
+      set.date,
+      set.expectedSet,
+      set.functionUnderTest,
+      set.expectedPropertyName
+    )
   );
 });
