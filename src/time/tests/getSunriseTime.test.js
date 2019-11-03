00d02d731751d5af2fafc813eb67cf0f2ab3e20e
Edonis Seferi <doni.seferi@gmail.com>
2019-11-03 16:59:52 +0000

    general test time method added

--- src/time/tests/getSunriseTime.test.js
+++ src/time/tests/getSunriseTime.test.js
@@ -1,37 +1,43 @@
-import { suite, test } from 'mocha';
+import { test } from 'mocha';
 import { assert } from 'chai';
-import { getSunriseDateTimeUtc } from '../../../index';
+import {
+  getSunriseDateTimeUtc,
+  getSunsetDateTimeUtc
+} from '../../../index';
 import januarySunriseExpected from './utcJanSunrise.json';
 import julySunriseExpected from './utcJulySunrise.json';
+import januarySunsetExpected from './utcJanSunset.json';
+import julySunsetExpected from './utcJulySunset.json';
 
-suite('Get Sunrise Time', () => {
-  test('returns sunrise time within a +/- 1 minute accuracy for the 01/01/2019', () => {
-    januarySunriseExpected.forEach(expected => {
-      const { ExpectedTimeUtc, Latitude, Longitude } = expected;
-      const expectedTime = new Date(ExpectedTimeUtc);
-      const actualDateTime = getSunriseDateTimeUtc(
-        new Date(2019, 0, 1),
-        Latitude,
-        Longitude
-      );
-      const differenceInSeconds =
-        (expectedTime.getTime() - actualDateTime.getTime()) / 1000;
-      assert.closeTo(differenceInSeconds, 0, 60);
-    });
+const testGeneric = (targetDate, testData, functionUnderTest) => {
+  testData.forEach(expected => {
+    const { ExpectedTimeUtc, Latitude, Longitude } = expected;
+    const expectedTime = new Date(ExpectedTimeUtc);
+    const actualDateTime = functionUnderTest(targetDate, Latitude, Longitude);
+    const differenceInSeconds =
+      (expectedTime.getTime() - actualDateTime.getTime()) / 1000;
+    assert.closeTo(differenceInSeconds, 0, 60);
   });
+};
 
-  test('returns sunset time within a +/- 1 minute accuracy for the 01/07/2019', () => {
-    julySunriseExpected.forEach(expected => {
-      const { ExpectedTimeUtc, Latitude, Longitude } = expected;
-      const expectedTime = new Date(ExpectedTimeUtc);
-      const actualTime = getSunriseDateTimeUtc(
-        new Date(2019, 6, 1),
-        Latitude,
-        Longitude
-      );
-      const differenceInSeconds =
-        (expectedTime.getTime() - actualTime.getTime()) / 1000;
-      assert.closeTo(differenceInSeconds, 0, 60);
-    });
-  });
+const januaryTestModel = (set, functionUnderTest) =>
+  model(new Date(2019, 0, 1), set, functionUnderTest);
+const julyTestModel = (set, functionUnderTest) =>
+  model(new Date(2019, 6, 1), set, functionUnderTest);
+const model = (date, set, functionUnderTest) => ({
+  date,
+  expectedSet: set,
+  functionUnderTest
+});
+
+test('returns times time within a +/- 1 minute accuracy', () => {
+  const testData = [
+    januaryTestModel(januarySunriseExpected, getSunriseDateTimeUtc),
+    julyTestModel(julySunriseExpected, getSunriseDateTimeUtc),
+    januaryTestModel(januarySunsetExpected, getSunsetDateTimeUtc),
+    julyTestModel(julySunsetExpected, getSunsetDateTimeUtc)
+  ];
+  testData.forEach(set =>
+    testGeneric(set.date, set.expectedSet, set.functionUnderTest)
+  );
 });
