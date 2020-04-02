eea0a12b44064f3683d7eaf4e6c3fd3f3b659407
Edonis Seferi <doni.seferi@gmail.com>
2020-04-02 16:16:31 +0100

    failing tests added

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -78,4 +78,64 @@ suite('Index', () => {
       suntimes.getCivilDuskStartDateTimeUtc(date, lat, lng)
     ).to.not.be.null.and.not.throw();
   });
+  test('Returns correct response when the sun is up all day for that location', () => {
+    [{
+      latitude: 82.518689,
+      longitude: -62.273199,
+      date: new Date(Date.UTC(2020, 5, 15))
+    }, {
+      latitude: 71.980070,
+      longitude: 102.474270,
+      date: new Date(Date.UTC(2037, 7, 2))
+    }, {
+      latitude: 78.222379,
+      longitude: 15.652313,
+      date: new Date(Date.UTC(2040, 3, 25))
+    }, {
+      latitude: -75.100620,
+      longitude: 123.354750,
+      date: new Date(Date.UTC(2002, 0, 27))
+    }].forEach(upAllDay => {
+      expect(suntimes.getSunriseDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getCivilDawnEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getCivilDawnStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getNauticalDawnEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getNauticalDawnStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getAstronomicalDawnEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getAstronomicalDawnStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getAstronomicalDuskStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getAstronomicalDuskEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getNauticalDuskStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getNauticalDuskEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getCivilDuskStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getCivilDuskEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+      expect(suntimes.getSunsetDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is up all day');
+    });
+  });
+  test('Returns correct response when the sun is down all day for that location', () => {
+    [{
+      latitude: 82.518689,
+      longitude: -62.273199,
+      date: new Date(Date.UTC(2020, 5, 15))
+    }, {
+      latitude: 71.980070,
+      longitude: 102.474270,
+      date: new Date(Date.UTC(2037, 7, 2))
+    }, {
+      latitude: 78.222379,
+      longitude: 15.652313,
+      date: new Date(Date.UTC(2040, 3, 25))
+    }].forEach(upAllDay => {
+      expect(suntimes.getSunriseDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is down all day');
+      expect(suntimes.getCivilDawnEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun does reach angle x°');
+      expect(suntimes.getCivilDawnStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun does reach angle x°');
+      expect(suntimes.getNauticalDawnEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun does reach angle x°');
+      expect(suntimes.getNauticalDawnStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun does reach angle x°');
+      expect(suntimes.getNauticalDuskStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun does reach angle x°');
+      expect(suntimes.getNauticalDuskEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun does reach angle x°');
+      expect(suntimes.getCivilDuskStartDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun does reach angle x°');
+      expect(suntimes.getCivilDuskEndDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun does reach angle x°');
+      expect(suntimes.getSunsetDateTimeUtc(upAllDay.date, upAllDay.latitude, upAllDay.longitude)).to.equal('The sun is down all day');
+    });
+  });
 });
