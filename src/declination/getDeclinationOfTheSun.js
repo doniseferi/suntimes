abb435d100c0ed208fbd3f2183c81a87a76ad839
Edonis Seferi <doni.seferi@gmail.com>
2020-04-08 20:53:07 +0100

    jsdocs moved

--- src/declination/getDeclinationOfTheSun.js
+++ src/declination/getDeclinationOfTheSun.js
@@ -52,7 +52,15 @@ const declinationOfTheSunFactory = (sine, cosine, arcsine, getDayOfTheYear) => {
   };
 
   return Object.freeze({
-    getDeclinationOfTheSun
+  /**
+* The angular distance of the sun north or south of the earth's equator.
+* @example <caption>Example usage of getDeclinationOfTheSun.</caption>
+* // returns -23.078780854838076
+* ```suntimes.getDeclinationOfTheSun(new Date(2019, 0, 1));```
+* @param {Date} date - A date instance
+* @returns {number} The declination in degrees where north is a positive value and south is a negative value within a range of range of -23.45 and 23.45.
+*/
+    getDeclinationOfTheSun: (date) => getDeclinationOfTheSun(date)
   });
 };
 
--- src/declination/index.js
+++ src/declination/index.js
@@ -2,14 +2,6 @@ import declinationOfTheSunFactory from './getDeclinationOfTheSun';
 import { sine, cosine, arcsine } from '../trigonometry/index';
 import getDayOfTheYear from '../dayOfTheYear/index';
 
-/**
-* The angular distance of the sun north or south of the earth's equator.
-* @example <caption>Example usage of getDeclinationOfTheSun.</caption>
-* // returns -23.078780854838076
-* ```suntimes.getDeclinationOfTheSun(new Date(2019, 0, 1));```
-* @param {Date} date - A date instance
-* @returns {number} The declination in degrees where north is a positive value and south is a negative value within a range of range of -23.45 and 23.45.
-*/
 const getDeclinationOfTheSun = declinationOfTheSunFactory(
   sine,
   cosine,
