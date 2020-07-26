a497663e42c43702e320496b2711c4f501f480d9
Edonis Seferi <doni.seferi@gmail.com>
2020-07-26 22:09:55 +0100

    security audit

--- src/perpetualEvents/errorMessages.js
+++ src/perpetualEvents/errorMessages.js
@@ -1,14 +1,14 @@
 const sunAltitudeAboveErrorMessage = (date, latitude, longitude, degrees) =>
-    `The sun's altitude does not drop to ${degrees}° on ${date.toDateString()} at latitude ${latitude} and longitude ${longitude}`;
+  `The sun's altitude does not drop to ${degrees}° on ${date.toDateString()} at latitude ${latitude} and longitude ${longitude}`;
 
 const sunAltitudeBelowErrorMessage = (date, latitude, longitude, degrees) =>
-    `The sun's altitude does not rise to ${degrees}° on ${date.toDateString()} at latitude ${latitude} and longitude ${longitude}`;
+  `The sun's altitude does not rise to ${degrees}° on ${date.toDateString()} at latitude ${latitude} and longitude ${longitude}`;
 
 const sunUpAllDayErrorMessage = (date, latitude, longitude) =>
-    `The sun is up all day on ${date.toDateString()} at latitude ${latitude} and longitude ${longitude}`;
+  `The sun is up all day on ${date.toDateString()} at latitude ${latitude} and longitude ${longitude}`;
 
 const sunDownAllDayErrorMessage = (date, latitude, longitude) =>
-    `The sun is down all day on ${date.toDateString()} at latitude ${latitude} and longitude ${longitude}`;
+  `The sun is down all day on ${date.toDateString()} at latitude ${latitude} and longitude ${longitude}`;
 
 export {
   sunAltitudeAboveErrorMessage,
